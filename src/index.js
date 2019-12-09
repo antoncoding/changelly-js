83129b5eea64709ece5c15445f65fe5a14b775d8
Anton Cheng <anton@coolbitx.com>
2019-12-09 10:40:15 +0800

    Add all changelly methods

--- src/index.js
+++ src/index.js
@@ -74,8 +74,30 @@ export class Changelly {
     return await this.postAPI('createTransaction', params)
   }
 
+  /**
+   * Returns status of a given transaction using a transaction ID provided.
+   * @param {string} id 
+   * @returns {Promise<string>}
+   */
+  async getStatus(id) {
+    const params = { id }
+    return await this.postAPI('getStatus', params)
+  }
 
+  /**
+   * Returns an array of all transactions or a filtered list of transactions
+   * @returns {Promise<Promise<Array<{ id: string, createdAt: number, type: string, moneyReceived: number, moneySent: number, rate: string, payinConfirmations: string, status: string, currencyFrom: string, currencyTo: string, payinAddress: string, payinExtraId: null|string, payinExtraIdName: null|string, payinHash: null|string, amountExpectedFrom: string, payoutAddress: string, payoutExtraId: null|string, payoutExtraIdName: null|string, payoutHash: null|string, refundHash: null|string, amountFrom: string, amountTo: string, amountExpectedTo: string, networkFee: string, changellyFee: string, apiExtraFee: string, totalFee: null|string, fiatProviderId: null|string, fiatProvider: null|string, fiatProviderRedirect: null|string }>>>}
+   */
+  async getTransactions() {
+    return await this.postAPI('getTransactions')
+  }
 
+  /**
+   * Sign params and post to Changelly server
+   * @param {string} method 
+   * @param {object} params
+   * @returns {Promise<any>}
+   */
   async postAPI(method, params={}) {
     return await request(method, params, this.apiKey, this.apiSecret)
   }
