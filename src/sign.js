63d004d376a8706de6d61d96847a130e6b51861a
Anton Cheng <anton@coolbitx.com>
2019-12-24 23:11:34 +0800

    Remove js

--- src/sign.js
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-export const signMessage = (message, apiSecret) => {
-  return crypto.createHmac('sha512', apiSecret)
-   .update(JSON.stringify(message))
-   .digest('hex');
-}
